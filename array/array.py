478d5f05b301b710556d906bf5ff5c26d2abaddd
Иван <ivan07m@gmail.com>
2021-11-19 10:00:28 +0300

    fix array

--- array/array.py
+++ array/array.py
@@ -16,28 +16,28 @@ class Array(list):
         'set': set
     }
 
-    def __new__(cls, array, limit=None, type=None):
-        if (len(array) > limit) or (False in (isinstance(i, cls.__type.get(type)) for i in array)):
-            raise Exception(f"max limit {limit}, type {type}")
+    def __new__(cls, array: list, __array_type: str, limit: int = None):
+        if (len(array) > limit) or (False in (isinstance(i, cls.__type.get(__array_type)) for i in array)):
+            raise Exception(f"max limit {limit}, type {__array_type}")
         return super().__new__(cls)
 
     def append(self, __object) -> None:
-        if (len(self) > self.__limit) or (isinstance(__object, self.__type.get(self.type)) is False):
-            raise Exception(f"max limit {self.__limit}, type {self.__type}")
+        if (len(self) > self.__limit) or (isinstance(__object, self.__type.get(self.array_type)) is False):
+            raise Exception(f"max limit {self.__limit}, type {self.__array_type}")
         return super().append(__object)
 
     def insert(self, __index: int, __object) -> None:
-        if (len(self) > self.__limit) or (isinstance(__object, self.__type.get(self.__type)) is False):
-            raise Exception(f"max limit {self.__limit}, type {self.__type}")
+        if (len(self) > self.__limit) or (isinstance(__object, self.__type.get(self.array_type)) is False):
+            raise Exception(f"max limit {self.__limit}, type {self.__array_type}")
         return super().insert(__index, __object)
 
     def extend(self, __iterable: list) -> None:
         if (len(self) + len(__iterable) > self.__limit) or (
-                False in (isinstance(i, self.__type.get(self.__type)) for i in __iterable)):
-            raise Exception(f"max limit {self.__limit}, type {self.__type}")
+                False in (isinstance(i, self.__type.get(self.array_type)) for i in __iterable)):
+            raise Exception(f"max limit {self.__limit}, type {self.__array_type}")
         return super().extend(__iterable)
 
-    def __init__(self, array, type, limit=None):
+    def __init__(self, array: list, array_type: str, limit: int = None):
         super().__init__(array)
         self.__limit = limit
-        self.type = type
+        self.__array_type = array_type
